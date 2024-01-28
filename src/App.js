945cad3826d520bd9e934b5af22d58e52cbe5ddd
Lim Zhi Xin <lim.zhixin@fazzfinancial.com>
2024-01-28 17:03:02 +0800

    Refactor to extract Square Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,28 @@
+function Square({ value }) {
+  return <button className="square">{value}</button>;
+}
+
 function Board() {
+  return (
+    <>
+      <div className="board-row" id="row1">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div><div className="board-row" id="row2">
+        <Square value="4"></Square>
+        <Square value="5"></Square>
+        <Square value="6"></Square>
+      </div><div className="board-row" id="row3">
+        <Square value="7"></Square>
+        <Square value="8"></Square>
+        <Square value="9"></Square>
+      </div>
+    </>
+  );
+}
+
+function Board2() {
   return (
     <>
       <div className="board-row" id="row1">

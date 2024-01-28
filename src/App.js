7c631a01565840a5fafe09cb5c0cd7780c766687
Lim Zhi Xin <lim.zhixin@fazzfinancial.com>
2024-01-28 16:58:23 +0800

    Create Board component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-function Square() {
-  return <button className="square">X</button>;
+function Board() {
+  return (
+    <>
+      <div className="board-row" id="row1">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+      </div><div className="board-row" id="row2">
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
+      </div><div className="board-row" id="row3">
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
+      </div>
+    </>
+  );
 }
-
-export default Square;
+export default Board;
